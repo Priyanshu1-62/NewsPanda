825b0b76188d03ca68a2b50dda04774796217aaf
Priyanshu1-62 <bpriyanshu5678@gmail.com>
2025-05-15 14:07:53 +0530

    Retry Deployment

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -6,7 +6,7 @@ const Navbar = () => {
       <>
 <nav className="navbar navbar-expand-lg  navbar-dark position-sticky" style={{zIndex: 1, top: 0, backgroundColor: "#0d1321"}}>
   <div className="container-fluid">
-    <a className="navbar-brand" href="/" style={{pointerEvents: "none"}}>Jai Jinendra</a>
+    <a className="navbar-brand" href="/">Jai Jinendra</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
